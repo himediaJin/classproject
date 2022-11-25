70c26eaff1d60e470e05cb128e7b00b064b7a5c0
Jin <113006967+himediaJin@users.noreply.github.com>
2022-11-25 12:39:02 +0900

    DEPT Validation

--- spring/intellij/Dept/src/main/java/com/app/manager/controller/dept/DeptRegController.java
+++ spring/intellij/Dept/src/main/java/com/app/manager/controller/dept/DeptRegController.java
@@ -5,9 +5,13 @@ import com.app.manager.service.dept.DeptRegService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @Log4j2
@@ -24,13 +28,22 @@ public class DeptRegController {
 
     @PostMapping
     public String reg(
-            DeptDTO deptDTO
+            @Valid DeptDTO deptDTO,
+            BindingResult bindingResult
     ){
 
         log.info("reg Post ...");
         log.info(deptDTO);
 
+        if(bindingResult.hasErrors()){
+            log.info(bindingResult.getAllErrors());
+            for(ObjectError objectError : bindingResult.getAllErrors()){
+                log.info(objectError.getCodes()[1] + " : " + objectError.getDefaultMessage());
+            }
+        }
+
         regService.insertDept(deptDTO);
+        //regService.insertDepts(deptDTO);
 
         return "redirect:/dept/list";
     }

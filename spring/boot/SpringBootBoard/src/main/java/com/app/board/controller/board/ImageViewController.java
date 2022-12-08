c3754f5eb0c072942f70bd2ab5c0838ee3254cd7
Jin <113006967+himediaJin@users.noreply.github.com>
2022-12-08 16:21:13 +0900

    Spring boot Board

--- spring/boot/SpringBootBoard/src/main/java/com/app/board/controller/board/ImageViewController.java
+++ spring/boot/SpringBootBoard/src/main/java/com/app/board/controller/board/ImageViewController.java
@@ -1,5 +1,6 @@
 package com.app.board.controller.board;
 
+import lombok.Cleanup;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -33,6 +34,7 @@ public class ImageViewController {
             // 응답 처리
             InputStream imageStream = new FileInputStream(savedFile);
             imageByteArray = imageStream.readAllBytes();
+            imageStream.close();
             status = HttpStatus.OK;
         }
 

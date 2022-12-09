a78566081ee3ee4d5c5c5fefed0d21de84746ee1
Jin <113006967+himediaJin@users.noreply.github.com>
2022-12-09 13:13:44 +0900

    Spring boot Board reply

--- spring/boot/SpringBootBoard/src/main/java/com/app/board/controller/board/ReplyRestController.java
+++ spring/boot/SpringBootBoard/src/main/java/com/app/board/controller/board/ReplyRestController.java
@@ -1,24 +1,32 @@
 package com.app.board.controller.board;
 
 import com.app.board.domain.ReplyDTO;
+import com.app.board.service.ReplyInsertService;
 import com.app.board.service.ReplyListService;
+import com.app.board.service.ReplyReadService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/reply")
+@Log4j2
 public class ReplyRestController {
 
     @Autowired
     private ReplyListService replyListService;
+
+    @Autowired
+    private ReplyInsertService replyInsertService;
+
+    @Autowired
+    private ReplyReadService replyReadService;
 
     // get  /reply/{bno} => list
     @GetMapping(value = "/{bno}",produces = MediaType.APPLICATION_JSON_VALUE)
@@ -34,7 +42,31 @@ public class ReplyRestController {
 
 
 
-    // post /reply => reply
+    // post /reply => reply    JSON 데이터를 받아서 DB insert
+    @PostMapping
+    public ResponseEntity<ReplyDTO> insertReply(
+        @RequestBody ReplyDTO replyDTO
+    ){
+
+        log.info("insert 전 : "+replyDTO);
+
+        // Service -> Mapper
+        replyInsertService.insertReply(replyDTO);
+
+        log.info("insert 후 : "+replyDTO);  // rno 값이 갱신된 데이터
+
+        //replyDTO.setReplydate(LocalDate.now().toString());
+
+        return new ResponseEntity<>(replyReadService.selectByRno(replyDTO.getRno()), HttpStatus.OK);
+
+    }
+
+
+
+
+
+
+
     // put  /reply/{rno} => reply
     // delete /reply/{rno} => 0 / 1, ok, fail
 
--- spring/boot/SpringBootBoard/src/main/java/com/app/board/mapper/ReplyMapper.java
+++ spring/boot/SpringBootBoard/src/main/java/com/app/board/mapper/ReplyMapper.java
@@ -1,7 +1,9 @@
 package com.app.board.mapper;
 
 import com.app.board.domain.ReplyDTO;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -11,6 +13,15 @@ public interface ReplyMapper {
 
     @Select("select * from tbl_reply where bno=#{bno}")
     List<ReplyDTO> selectAll(int bno);
+
+
+    @Select("select * from tbl_reply where rno=#{rno}")
+    ReplyDTO selectByRno(int rno);
+
+
+    @Insert("INSERT INTO tbl_reply (bno,reply,replyer) VALUES (#{bno}, #{reply}, #{replyer})")
+    @Options(useGeneratedKeys = true, keyProperty = "rno", keyColumn = "rno")
+    int insertReply(ReplyDTO replyDTO);
 
 
 

c057ee8f86fe2ae74130b3738d7392efffc88770
Jin <113006967+himediaJin@users.noreply.github.com>
2022-12-28 18:06:42 +0900

    React

---
+++ react/blog/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './default.css';
+import BlogMain from "./component/BlogMain";
+
+function App() {
+  return (
+    <BlogMain></BlogMain>
+  );
+}
+
+export default App;

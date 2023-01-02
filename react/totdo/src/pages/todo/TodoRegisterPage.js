e4859b2ca4512342429bcd769c2abab523f9f08c
Jin <113006967+himediaJin@users.noreply.github.com>
2023-01-02 11:22:09 +0900

    React Todo 2023.01.02 1120

---
+++ react/totdo/src/pages/todo/TodoRegisterPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Grid, Paper} from "@mui/material";
+import PageLayout from "../../layout/PageLayout";
+import {useParams} from "react-router-dom";
+import Typography from "@mui/material/Typography";
+
+function TodoRegisterPage(props) {
+
+
+
+    return (
+        <PageLayout title={'Todo Register Page'}>
+            <Grid item xs={12} md={12} lg={12}>
+                <Paper sx={{p:2, display:'flex',flexDirection: 'column'}}>
+                    <h1>Todo Register Page</h1>
+                </Paper>
+            </Grid>
+        </PageLayout>
+    );
+}
+
+export default TodoRegisterPage;
